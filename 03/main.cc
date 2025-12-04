6a83c5b25cdddada786c0aa513ebc37d17834bc9
Dan Green <danngreen1@gmail.com>
2025-12-04 08:04:29 -0800

    Day 03 part 2

--- 03/main.cc
+++ 03/main.cc
@@ -4,26 +4,38 @@
 #include <iostream>
 #include <numeric>
 
-int max_pair_digits(std::string_view s) {
+template<size_t N>
+constexpr auto max_N_digits(std::string_view s) {
 
-	auto digit1 = std::max_element(s.begin(), s.end() - 1);
-	auto digit2 = std::max_element(digit1 + 1, s.end());
+	unsigned long joltage = 0;
+	auto digit = s.begin();
+	for (auto n = 0u; n < N; n++) {
+		digit = std::max_element(digit + (n != 0), s.end() - (N - n) + 1);
+		auto num = *digit - '0';
+		joltage = joltage * 10 + num;
+	}
 
-	auto num1 = *digit1 - '0';
-	auto num2 = *digit2 - '0';
-	// printf("%c%c\n", *digit1, *digit2);
-	return num1 * 10 + num2;
+	return joltage;
 }
+
+// Sample data:
+static_assert(max_N_digits<2>("98765432111111") == 98);
+static_assert(max_N_digits<2>("811111111111119") == 89);
+static_assert(max_N_digits<2>("234234234234278") == 78);
+static_assert(max_N_digits<2>("818181911112111") == 92);
+static_assert(max_N_digits<12>("98765432111111") == 987654321111);
+static_assert(max_N_digits<12>("811111111111119") == 811111111119);
+static_assert(max_N_digits<12>("234234234234278") == 434234234278);
+static_assert(max_N_digits<12>("818181911112111") == 888911112111);
 
 int main() {
 	unsigned long cnt;
-
-	cnt = std::accumulate(
-		sample_data.begin(), sample_data.end(), 0ul, [](auto sum, auto val) { return sum + max_pair_digits(val); });
-
-	std::cout << "Sample Part 1: " << cnt << "\n";
-
-	cnt = std::accumulate(data.begin(), data.end(), 0ul, [](auto sum, auto val) { return sum + max_pair_digits(val); });
+	cnt = std::accumulate(data.begin(), data.end(), 0ul, [](auto sum, auto val) { return sum + max_N_digits<2>(val); });
 
 	std::cout << "Part 1: " << cnt << "\n";
+
+	cnt =
+		std::accumulate(data.begin(), data.end(), 0ul, [](auto sum, auto val) { return sum + max_N_digits<12>(val); });
+
+	std::cout << "Part 2: " << cnt << "\n";
 }
