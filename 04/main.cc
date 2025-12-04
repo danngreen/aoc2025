92f94fbe300902061831018cfc8181f92768028a
Dan Green <danngreen1@gmail.com>
2025-12-04 08:56:51 -0800

    Day 04 part 2

--- 04/main.cc
+++ 04/main.cc
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <iostream>
 #include <span>
+#include <vector>
 
 constexpr bool filled(char c) {
 	return c != '.';
@@ -62,10 +63,56 @@ constexpr unsigned long count_accessible(const std::span<const std::string_view>
 	return free_rolls;
 }
 
+constexpr unsigned long remove_accessible(std::vector<std::string> &map) {
+	unsigned long free_rolls = 0;
+
+	for (unsigned y = 0; auto &line : map) {
+		for (unsigned x = 0; auto c : line) {
+			if (filled(c)) {
+
+				std::string prev = y > 0 ? map[y - 1] : "";
+				std::string next = y < map.size() - 1 ? map[y + 1] : "";
+				if (count_obstacles(prev, line, next, x) < 4) {
+					free_rolls++;
+					line[x] = '.';
+				}
+			}
+			x++;
+		}
+		y++;
+	}
+	return free_rolls;
+}
+
+constexpr unsigned long recursive_remove(std::vector<std::string> &map) {
+	unsigned long total = 0;
+	while (true) {
+		auto rolls = remove_accessible(map);
+		if (rolls == 0)
+			break;
+		total += rolls;
+	}
+	return total;
+}
+
+std::vector<std::string> copy_map(const std::span<const std::string_view> map) {
+	std::vector<std::string> m;
+	for (auto &line : map) {
+		m.push_back(std::string(line));
+	}
+	return m;
+}
+
 int main() {
 	unsigned long cnt;
 
 	std::cout << "Sample Part 1: " << count_accessible(sample_data) << "\n";
 
 	std::cout << "Part 1: " << count_accessible(data) << "\n";
+
+	auto sample2 = copy_map(sample_data);
+	std::cout << "Sample Part 2: " << recursive_remove(sample2) << "\n";
+
+	auto data2 = copy_map(data);
+	std::cout << "Part 2: " << recursive_remove(data2) << "\n";
 }
