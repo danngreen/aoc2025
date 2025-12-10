96875c9bd40eb1f200277bcadfccb369d9bf36c6
Dan Green <danngreen1@gmail.com>
2025-12-09 21:21:25 -0800

    Day 8 part 1

--- 08/main.cc
+++ 08/main.cc
@@ -3,8 +3,8 @@
 #include <algorithm>
 #include <cstddef>
 #include <iostream>
+#include <set>
 #include <span>
-#include <unordered_set>
 #include <vector>
 
 // squared distance, no need to waste cycles on sqrt()
@@ -17,29 +17,28 @@ constexpr long distsq(Pos a, Pos b) {
 
 template<size_t N>
 using DistanceMatrix = std::array<long, N * N>;
-// using Matrix = std::array<std::array<long, N>, N>;
 
 template<size_t N>
-DistanceMatrix<N> create_matrix(std::array<Pos, N> const &pos) {
+constexpr DistanceMatrix<N> create_matrix(std::array<Pos, N> const &pos) {
 	DistanceMatrix<N> distances;
 
 	for (auto x = 0u; auto a : pos) {
 		for (auto y = 0u; auto b : pos) {
 			long idx = x + N * y;
 			if (x < y) {
-				printf("[%d] %d %d %d -> [%d] %d %d %d = [%ld] [%ld %ld] %ld\n",
-					   x,
-					   a.x,
-					   a.y,
-					   a.z,
-					   y,
-					   b.x,
-					   b.y,
-					   b.z,
-					   idx,
-					   idx % N,
-					   idx / N,
-					   distsq(a, b));
+				// printf("[%d] %d %d %d -> [%d] %d %d %d = [%ld] [%ld %ld] %ld\n",
+				// 	   x,
+				// 	   a.x,
+				// 	   a.y,
+				// 	   a.z,
+				// 	   y,
+				// 	   b.x,
+				// 	   b.y,
+				// 	   b.z,
+				// 	   idx,
+				// 	   idx % N,
+				// 	   idx / N,
+				// 	   distsq(a, b));
 				distances[idx] = distsq(a, b);
 			} else
 				distances[idx] = LONG_MAX;
@@ -47,103 +46,82 @@ DistanceMatrix<N> create_matrix(std::array<Pos, N> const &pos) {
 		}
 		x++;
 	}
+
 	return distances;
 }
 
-template<size_t N>
-std::vector<std::set<int>> connect_n_wires(int n, DistanceMatrix<N> &distances) {
-	std::vector<std::set<int>> circuits;
-
-	int wires = 0;
-
-	while (wires < n) {
-		auto closest = std::min_element(distances.begin(), distances.end());
-		auto dist = *closest;
-		auto p = std::distance(distances.begin(), closest);
-		*closest = LONG_MAX;
-		printf("%d-th shortest wire: between [%ld] and [%ld] = distance %ld==>", wires, p % N, p / N, dist);
-		int b = p % N;
-		int a = p / N;
-
-		// Add or append it to the circuit
-		bool found = false;
-		for (auto &cir : circuits) {
-			if (cir.contains(a) && cir.contains(b)) {
-				found = true;
-				printf("Skip, already in ");
-				for (auto c : cir)
-					printf("%d ", c);
-				printf("\n");
-				//do not count this wire
-				break;
-			}
-
-			else if (cir.contains(a) || cir.contains(b))
-			{
-				cir.insert(a);
-				cir.insert(b);
-				printf("Appended: size=>%zu: ", cir.size());
-				for (auto c : cir)
-					printf("%d ", c);
-				printf("\n");
-				found = true;
-				wires++;
-				break;
-			}
-		}
-
-		if (!found) {
-			auto &newcir = circuits.emplace_back();
-			printf("New circuit (sz 2)\n");
-			newcir.insert(a);
-			newcir.insert(b);
-			wires++;
-		}
-
-		if (num_pairs < 0)
-			break;
+void print_groups(std::span<int> groups) {
+	for (int i = 0; auto g : groups) {
+		printf("[%d] in group %d\n", i, g);
+		i++;
 	}
-
-	return circuits;
 }
 
 template<size_t N>
-std::array<int, N> calc_circuit_sizes(auto const &cirs) {
-	std::array<int, N> szs{};
+constexpr std::pair<int, int> get_closest(DistanceMatrix<N> &distances) {
+	auto closest = std::min_element(distances.begin(), distances.end());
+	auto dist = *closest;
+	auto p = std::distance(distances.begin(), closest);
+	*closest = LONG_MAX;
+	// printf("between [%ld] and [%ld] = distance %ld\n", p % N, p / N, dist);
+	int b = p % N;
+	int a = p / N;
+	return {a, b};
+}
 
-	int sz_i = 0;
-	for (auto &cir : cirs) {
-		szs[sz_i++] = cir.size();
+template<size_t N>
+constexpr std::array<int, N> group_boxes(int num_wires, DistanceMatrix<N> &distances) {
+	std::array<int, N> groups;
+
+	for (int i = 0; auto &g : groups) {
+		g = i++;
 	}
+	// print_groups(groups);
 
-	return szs;
+	for (int i = 0; i < num_wires; i++) {
+		// printf("\nWire %d\n", i);
+		auto [a, b] = get_closest<N>(distances);
+		auto group_a = groups[a];
+		auto group_b = groups[b];
+		for (int j = 0; auto &g : groups) {
+			// move all boxes that are in b's groups, into a's group
+			if (g == group_b && g != group_a) {
+				// printf("Move [%d] from group %d to %d\n", j, group_b, group_a);
+				g = group_a;
+			}
+			j++;
+		}
+		// print_groups(groups);
+	}
+	return groups;
+}
+
+template<size_t N>
+constexpr std::array<int, N> count_group_sizes(std::span<int> groups) {
+	std::array<int, N> sizes{};
+	for (auto g : groups) {
+		sizes[g]++;
+	}
+	std::sort(sizes.rbegin(), sizes.rend());
+	return sizes;
 }
 
 int main() {
 	{
 		auto matrix = create_matrix(sample_data);
-		auto circuits = connect_n_wires<20>(10, matrix);
-
-		std::partial_sort(circuits.begin(), circuits.begin() + 3, circuits.end(), [](auto &a, auto &b) {
-			return a.size() > b.size();
-		});
-
-		std::cout << circuits[0].size() << "*" << circuits[1].size() << "*" << circuits[2].size() << "="
-				  << circuits[0].size() * circuits[1].size() * circuits[2].size() << "\n";
+		auto groups = group_boxes<20>(10, matrix);
+		auto sizes = count_group_sizes<20>(groups);
+		std::cout << "Sample part 1: " << sizes[0] << " * " << sizes[1] << " * " << sizes[2] << " = "
+				  << (sizes[0] * sizes[1] * sizes[2]) << "\n";
 	}
-
 	{
 		auto matrix = create_matrix(data);
-		auto circuits = connect_n_wires<1000>(1000, matrix);
-
-		std::partial_sort(circuits.begin(), circuits.begin() + 3, circuits.end(), [](auto &a, auto &b) {
-			return a.size() > b.size();
-		});
-
-		std::cout << circuits[0].size() << "*" << circuits[1].size() << "*" << circuits[2].size() << "="
-				  << circuits[0].size() * circuits[1].size() * circuits[2].size() << "\n";
-
-		//13*12*12 = 2028 if we count wires that connect existing circuits without adding an element
+		auto groups = group_boxes<1000>(1000, matrix);
+		auto sizes = count_group_sizes<1000>(groups);
+		std::cout << "Part 1: " << sizes[0] << " * " << sizes[1] << " * " << sizes[2] << " = "
+				  << (sizes[0] * sizes[1] * sizes[2]) << "\n";
+	}
+	{
 		//18*17*16 = 4896 too low
 	}
 }
